70a03bbecab8c26c01165688460acd6498c0897d
thaigialai1987 <thaigialai1987@gmail.com>
2021-12-31 10:49:47 +0700

    public

---
+++ config - Copy.js
@@ -0,0 +1,16 @@
+/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	config.extraPlugins = 'wordcount,notification,lineheight,preview,letterspacing,html5video,uploadfile';
+	config.height = 500;
+	config.width = 'auto';
+	config.removeButtons = 'Save';
+};
+
+
